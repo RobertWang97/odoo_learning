8439dcb663de916549c1fe3949dab86ea30249c1
Robert Wang <wtaodlw@cn.ibm.com>
2025-04-16 10:33:57 +0800

    leanring3

---
+++ my_addons/my_learning3/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'learning3',
+    'summary': 'my learning3',
+    'description': 'Start Learning',
+    'version': '1.0',
+    'author': 'Robert Wang',
+    'license': 'AGPL-3',
+    # 'website': 'todo',
+    # 'category': 'Tools',
+    'depends': ['base', ''],
+    'data': ['security/ir.model.access.csv', 'views/learning3_record_view.xml'],
+    'qweb': [],
+    'depends': [],
+    'installable': True,
+    'application': True
+}
