05039b27337026df03586754a74d9919e5a1e9d2
Robert Wang <wtaodlw@cn.ibm.com>
2025-04-19 09:45:55 +0800

    leanring3

--- my_addons/my_learning3/models/my_learning3.py
+++ my_addons/my_learning3/models/my_learning3.py
@@ -3,7 +3,11 @@ from odoo import api, fields, models
 
 class MyLearning3(models.Model):
     _name = 'my_learning3'
+    _description = 'learning 3'
     name = fields.Char(string='Name', required=True)
-    title = fields.Char(string='Title', required=True)
+    title = fields.Char(string='Title')
     description = fields.Text(string='Description')
     radio1 = fields.Selection(selection=[('a', '1'), ('b', '2'), ('c', '3')], string='Choice 1', required=True)
+    currency_id = fields.Many2one('res.currency', string='Currency')
+    #optional attribute: currency_field='currency_id'
+    value = fields.Monetary(string='total')
