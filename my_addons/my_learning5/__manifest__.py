917e1a533494bb0372788f697123960e54757d18
Robert Wang <wtaodlw@cn.ibm.com>
2025-04-29 11:49:44 +0800

    leanring5

---
+++ my_addons/my_learning5/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'learning5',
+    'summary': 'my learning5',
+    'description': 'Start Learning',
+    'version': '1.0',
+    'author': 'Robert Wang',
+    'license': 'AGPL-3',
+    # 'website': 'todo',
+    # 'category': 'Tools',
+    'depends': ['base', ''],
+    'data': ['security/ir.model.access.csv', 'views/learning5_record_view.xml'],
+    'qweb': [],
+    'depends': [],
+    'installable': True,
+    'application': True
+}
